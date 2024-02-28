d4aa04a46b5444dea94c8e6b5027f00c84d8dd72
DevPDR <pedrojulianoquimelo@outlook.com>
2024-02-27 21:52:24 -0300

    Desenhando a tela de listagem

---
+++ src/screens/taskList.js
@@ -0,0 +1,60 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import todayImage from '../../assets/imgs/today.jpg';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import commonStyles from '../commonStyles';
+import { SafeAreaView } from 'react-navigation';
+
+export default class TaskList extends Component {
+  render() {
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
+
+
+    return (
+      <View style={style.container}>
+        <ImageBackground style={style.backgound} source={todayImage} >
+          <View style={style.titleBar}>
+            <Text style={style.title}>Hoje</Text>
+            <Text style={style.subtitle}>{today}</Text>
+          </View>
+        </ImageBackground>
+        <View style={style.taskList}>
+          <Text>Tarefa #01</Text>
+
+        </View>
+      </View>
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgound: {
+    flex: 3,
+  },
+  taskList: {
+    flex: 7,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+});
